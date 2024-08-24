abaa5079512d39b98519ba15c9d05fef5ecc9c96
Srinu-7 <srinumaka9908@gmail.com>
2024-08-24 19:43:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const codes = document.querySelectorAll('.code');
 codes.forEach((code, idx) => {
     code.addEventListener('keydown', (e) => {
         if (e.key >= 0 && e.key <= 9) {
-            codes[idx].value = ''; // Clear the input
+            codes[idx].value = ''; // Clear the input before entering the new value
             setTimeout(() => codes[idx + 1]?.focus(), 10); // Move to the next input
         } else if (e.key === 'Backspace') {
+            codes[idx].value = ''; // Clear the current input
             setTimeout(() => codes[idx - 1]?.focus(), 10); // Move to the previous input
         }
     });
